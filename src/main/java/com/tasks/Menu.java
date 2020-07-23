b272500c7f21547e0a58ec1e7078b033564ea773
ditrim023 <krutoguz@cloudwharf.com>
2020-07-23 18:20:53 +0300

    clean code

--- src/main/java/com/tasks/Menu.java
+++ src/main/java/com/tasks/Menu.java
@@ -2,14 +2,14 @@ package com.tasks;
 
 
 import com.tasks.cities.PathFinder;
+import com.tasks.factorial.Factorial;
 import com.tasks.parentheses.Parentheses;
 
 public class Menu {
     public static void main(String[] args) {
-//        Factorial factorial = new Factorial();
+        Factorial factorial = new Factorial();
         Parentheses parentheses = new Parentheses();
         PathFinder pathFinder = new PathFinder();
         pathFinder.run();
-//        parentheses.run();
     }
 }
