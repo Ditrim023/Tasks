a56a00a9dd36dfe7c435d8668f30107b2f861d13
ditrim023 <krutoguz@cloudwharf.com>
2020-07-23 19:31:00 +0300

    add Menu

---
+++ src/main/java/com/tasks/Main.java
@@ -0,0 +1,8 @@
+package com.tasks;
+
+public class Main {
+    public static void main(String[] args) {
+           Menu menu = new Menu();
+           menu.runMenu();
+    }
+}
--- src/main/java/com/tasks/Menu.java
+++ src/main/java/com/tasks/Menu.java
@@ -5,11 +5,38 @@ import com.tasks.cities.PathFinder;
 import com.tasks.factorial.Factorial;
 import com.tasks.parentheses.Parentheses;
 
+import java.util.Scanner;
+
 public class Menu {
-    public static void main(String[] args) {
-        Factorial factorial = new Factorial();
-        Parentheses parentheses = new Parentheses();
-        PathFinder pathFinder = new PathFinder();
-        pathFinder.run();
+    private static final String OPERATION = "Choose number of task \n 1 - Parentheses \n 2 - Find way \n 3 - Factorial or something else to exit";
+    private Factorial factorial = new Factorial();
+    private Parentheses parentheses = new Parentheses();
+    private PathFinder pathFinder = new PathFinder();
+    private Scanner scanner = new Scanner(System.in);
+
+    public void runMenu() {
+        printMessage(OPERATION);
+        String operationNumber = nextLine();
+        switch (operationNumber) {
+            case "3":
+                factorial.run();
+                break;
+            case "1":
+                parentheses.run();
+                break;
+            case "2":
+                pathFinder.run();
+                break;
+            default: break;
+        }
     }
+
+    private void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    private String nextLine() {
+        return scanner.nextLine();
+    }
+
 }
